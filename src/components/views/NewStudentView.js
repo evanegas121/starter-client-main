e15ed2859ad454b828ba2cf0dd70ec084d7b2904
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-12-21 02:24:15 -0500

    student

--- src/components/views/NewStudentView.js
+++ src/components/views/NewStudentView.js
@@ -1,34 +1,40 @@
-import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles( () => ({
-  formContainer:{  
-    width: '500px',
-    backgroundColor: '#f0f0f5',
-    borderRadius: '5px',
-    margin: 'auto',
+  root: {
+    flexGrow: 1,
   },
   title: {
     flexGrow: 1,
     textAlign: 'left',
-    textDecoration: 'none'
-  }, 
-  customizeAppBar:{
+    fontType: 'bold',
+    fontFamily: 'Courier, sans-serif', 
+    fontSize: '35px', 
+    color: '#CDDC39'
+  },
+  appBar:{
     backgroundColor: '#11153e',
     shadows: ['none'],
   },
-  formTitle:{
-    backgroundColor:'#c5c8d6',
-    marginBottom: '15px',
-    textAlign: 'center',
-    borderRadius: '5px 5px 0px 0px',
-    padding: '3px'
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: "50%",
+    margin: "auto",
   },
-  
+  links:{
+    textDecoration: 'none',
+  }
+
 }));
 
 const NewStudentView = (props) => {
@@ -36,8 +42,27 @@ const NewStudentView = (props) => {
   const classes = useStyles();
 
   return (
+    <><AppBar position="static" elevation={0} className={classes.appBar}>
+    <Toolbar>
+    <Link variant="h6" className={classes.title} to={'/'} color="inherit" >
+        Home
+      </Link>
+
+      <Link className={classes.links} to={'/campuses'} >
+        <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+          All Campuses
+        </Button>
+      </Link>
+
+      <Link className={classes.links} to={'/students'} >
+        <Button variant="contained" color="primary">
+          All Students
+        </Button>
+      </Link>
+    </Toolbar>
+  </AppBar>
     <div className={classes.root}>
-      <div className={classes.formContainer}>
+      <div className="formcontainer">
         <div className={classes.formTitle}>
           <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
             New Student
@@ -45,29 +70,29 @@ const NewStudentView = (props) => {
         </div>
         <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
           <label style= {{color:'#11153e', fontWeight: 'bold'}}>First Name: </label>
-          <input type="text" name="firstname" onChange ={(e) => handleChange(e)} />
+          <input type="text" name="firstname" required onChange ={(e) => handleChange(e)} />
           <br/>
           <br/>
 
           <label style={{color:'#11153e', fontWeight: 'bold'}}>Last Name: </label>
-          <input type="text" name="lastname" onChange={(e) => handleChange(e)} />
-          <br/>
-          <br/>
-
-          <label style={{color:'#11153e', fontWeight: 'bold'}}>GPA: </label>
-          <input type="text" name="gpa" onChange={(e) => handleChange(e)} />
+          <input type="text" name="lastname" required onChange={(e) => handleChange(e)} />
           <br/>
           <br/>
 
           <label style={{color:'#11153e', fontWeight: 'bold'}}>Email: </label>
-          <input type="text" name="email" onChange={(e) => handleChange(e)} />
+          <input type="email" name="email" required onChange={(e) => handleChange(e)} />
           <br/>
           <br/>
 
-          {/* <label style={{color:'#11153e', fontWeight: 'bold'}}>campusId: </label>
+          <label style={{color:'#11153e', fontWeight: 'bold'}}>campusId: </label>
           <input type="text" name="campusId" onChange={(e) => handleChange(e)} />
           <br/>
-          <br/> */}
+          <br/>
+
+          <label style={{color:'#11153e', fontWeight: 'bold'}}>GPA: </label>
+          <input type="number" name="gpa" min="0" max="4" step="0.001" onChange={(e) => handleChange(e)} />
+          <br/>
+          <br/>
 
           <Button variant="contained" color="primary" type="submit">
             Submit
@@ -76,8 +101,8 @@ const NewStudentView = (props) => {
           <br/>
         </form>
         </div>
-      </div>
-    
+      </div></>
+
   )
 }
 
